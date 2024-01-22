f67ba2769b575cef0a6220c7b66bdee4450db1c7
Hai Tran <tranvanhai240x@gmail.com>
2024-01-22 22:32:11 +0700

    update relative path

--- js/index.js
+++ js/index.js
@@ -1,42 +1,40 @@
 $(document).ready(() => {
-  $(".regular").slick({
-    dots: true,
+  $(".slider-talent").slick({
     infinite: true,
-    slidesToShow: 6,
-    slidesToScroll: 6,
-    autoplay: true,
-    autoplaySpeed: 2000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
     responsive: [
       {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 4,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 480,
+        breakpoint: 992,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: true,
-          dots: true,
         },
       },
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+  $(".slider-insights").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
     ],
   });
   scroolDown();
